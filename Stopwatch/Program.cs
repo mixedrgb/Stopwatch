156f3a01d256bcf1ccaf55570d1c8a72fcb648fb
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-09-17 23:59:55 -0500

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private static DateTime _startTime;
+        private static TimeSpan _stopTime;
+        private const string _programVersion = "v0.01";
+
+        private static void Start()
+        {
+            _startTime = DateTime.Now;
+        }
+
+        private static void Stop()
+        {
+            _stopTime = DateTime.Now - _startTime;
+        }
+
+        internal static void Main()
+        {
+            Console.WriteLine($"Stopwatch {_programVersion}");
+            Start();
+            Thread.Sleep(new Random().Next(0,2000));
+            Stop();
+            Console.WriteLine("Total lapsed time: {0}", _stopTime);
+        }
+    }
+}
