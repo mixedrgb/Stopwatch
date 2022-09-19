c05cd75c66ef4eb4fcd0cc324c3f9edab0234772
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-09-18 20:24:38 -0500

    The stopwatch is functional.

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -4,27 +4,89 @@ namespace Stopwatch
 {
     public class Stopwatch
     {
+        private static DateTime _stopTime;
         private static DateTime _startTime;
-        private static TimeSpan _stopTime;
-        private const string _programVersion = "v0.01";
+        private static bool _isStarted;
+        private const string ProgramVersion = "v0.01";
 
         private static void Start()
         {
-            _startTime = DateTime.Now;
+            if (!_isStarted)
+            {
+                _isStarted = true;
+                _startTime = DateTime.Now;
+                Console.WriteLine("Stopwatch started at {0}", _startTime);
+            }
+            else
+            {
+                //Console.WriteLine("Already started!");
+                var e = new InvalidOperationException();
+                Console.WriteLine("Already started!\nError: {0}", e.Message);
+            }
         }
 
         private static void Stop()
         {
-            _stopTime = DateTime.Now - _startTime;
+            if (_isStarted)
+            {
+                _isStarted = false;
+                _stopTime = DateTime.Now;
+                Console.WriteLine("Stopwatch stopped at {0}", _stopTime);
+            }
+            else
+            {
+                Console.WriteLine("Already stopped!");
+            }
+        }
+
+        private static void ShowTime()
+        {
+            if (_isStarted)
+            {
+                Console.WriteLine("Can't show time until stopped.");
+            }
+            else
+            {
+                Console.WriteLine("Total time lapsed was {0}", _stopTime - _startTime);
+            }
+        }
+
+        private static void Commands(string? command)
+        {
+            switch (command)
+            {
+                case "start":
+                    Start();
+                    break;
+                case "stop":
+                    Stop();
+                    break;
+                case "show":
+                    ShowTime();
+                    break;
+                case "exit":
+                    Console.WriteLine("Seeya, bro!");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No command \"{0}\" available.", command);
+                    break;
+            }
         }
 
         internal static void Main()
         {
-            Console.WriteLine($"Stopwatch {_programVersion}");
-            Start();
-            Thread.Sleep(new Random().Next(0,2000));
-            Stop();
-            Console.WriteLine("Total lapsed time: {0}", _stopTime);
+            string command = "";
+            Console.WriteLine($"Stopwatch {ProgramVersion}");
+            Console.WriteLine("When you're ready.");
+            while (true)
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                Commands(command);
+            }
+            //Thread.Sleep(new Random().Next(0,2000));
         }
+
     }
 }
