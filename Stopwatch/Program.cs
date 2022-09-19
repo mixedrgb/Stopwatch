2e7cad860b318d4d4eec457d58b373716837bb67
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-09-18 21:13:01 -0500

    Toasted Stopwatch: Now with 75% more object-orientedness!

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -9,81 +9,73 @@ namespace Stopwatch
         private static bool _isStarted;
         private const string ProgramVersion = "v0.01";
 
-        private static void Start()
+        public void Start()
+        {
+            if (_isStarted)
+                throw new InvalidOperationException("nO!");
+
+            _isStarted = true;
+            _startTime = DateTime.Now;
+        }
+
+        public void Stop()
         {
             if (!_isStarted)
-            {
-                _isStarted = true;
-                _startTime = DateTime.Now;
-                Console.WriteLine("Stopwatch started at {0}", _startTime);
-            }
-            else
-            {
-                //Console.WriteLine("Already started!");
-                var e = new InvalidOperationException();
-                Console.WriteLine("Already started!\nError: {0}", e.Message);
-            }
+                throw new InvalidOperationException("nO!");
+
+            _isStarted = false;
+            _stopTime = DateTime.Now;
         }
 
-        private static void Stop()
+        private TimeSpan ShowTime()
         {
-            if (_isStarted)
-            {
-                _isStarted = false;
-                _stopTime = DateTime.Now;
-                Console.WriteLine("Stopwatch stopped at {0}", _stopTime);
-            }
-            else
-            {
-                Console.WriteLine("Already stopped!");
-            }
+            return _stopTime - _startTime;
         }
 
-        private static void ShowTime()
-        {
-            if (_isStarted)
-            {
-                Console.WriteLine("Can't show time until stopped.");
-            }
-            else
-            {
-                Console.WriteLine("Total time lapsed was {0}", _stopTime - _startTime);
-            }
-        }
-
-        private static void ParseCommands(string? command)
-        {
-            switch (command?.ToLower())
-            {
-                case "start":
-                    Start();
-                    break;
-                case "stop":
-                    Stop();
-                    break;
-                case "show":
-                    ShowTime();
-                    break;
-                case "exit":
-                    Console.WriteLine("Seeya, bro!");
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("No command \"{0}\" available.", command);
-                    break;
-            }
-        }
+//        private static void ParseCommands(string? command)
+//        {
+//            switch (command?.ToLower())
+//            {
+//                case "start":
+//                    Start();
+//                    break;
+//                case "stop":
+//                    Stop();
+//                    break;
+//                case "show":
+//                    ShowTime();
+//                    break;
+//                case "exit":
+//                    Console.WriteLine("Seeya, bro!");
+//                    Environment.Exit(0);
+//                    break;
+//                default:
+//                    Console.WriteLine("No command \"{0}\" available.", command);
+//                    break;
+//            }
+//        }
 
         internal static void Main()
         {
             Console.WriteLine($"Stopwatch {ProgramVersion}");
-            Console.WriteLine("When you're ready.");
+
+            var stopwatch = new Stopwatch();
+
             while (true)
             {
-                Console.Write("> ");
-                var command = Console.ReadLine();
-                ParseCommands(command);
+                var randomNumber = new Random();
+                stopwatch.Start();
+                Thread.Sleep(randomNumber.Next(1, 2000));
+                stopwatch.Stop();
+                Console.WriteLine("Duration: {0}", stopwatch.ShowTime());
             }
+//            Console.WriteLine("When you're ready.");
+//            while (true)
+//            {
+//                Console.Write("> ");
+//                var command = Console.ReadLine();
+//                ParseCommands(command);
+//            }
         }
     }
 }
